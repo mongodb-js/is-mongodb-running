15eca86d5e31e061c9c1b48b8c8a19d88a5793b3
Addison Elliott <addison.elliott@gmail.com>
2018-04-25 17:37:57 -0500

    Update homepage and fix eslint errors and warnings (#6)
    * Update homepage and fix eslint errors and warnings
    
    Update homepage to be the GitHub page. Previously set to mongodb-js GitHub pages link that was invalid.
    
    When running eslint checks, the following errors would occur:
    
    ```
    1:1  error  Rule 'space-after-keywords' was removed and replaced by: keyword-spacing  space-after-keywords
    ```
    
    This was fixed in version 2.1.0 of eslint-mongodb-js package. Just needed updated.
    
    Remove unused module.
    
    Clean up no-console warnings with single-line eslint disables. The console seems appropiate here.
    
    * Add vscode directory to gitignore

--- bin/is-mongodb-running.js
+++ bin/is-mongodb-running.js
@@ -5,7 +5,6 @@ var path = require('path');
 var fs = require('fs');
 var chalk = require('chalk');
 var figures = require('figures');
-var format = require('util').format;
 
 var usage = fs.readFileSync(path.resolve(__dirname, '../usage.txt')).toString();
 var args = require('minimist')(process.argv.slice(2), {
@@ -19,11 +18,11 @@ var lookup = require('../');
 var pkg = require('../package.json');
 
 if (args.help || args.h) {
-  console.error(usage);
+  console.error(usage); // eslint-disable-line no-console
   process.exit(1);
 }
 if (args.version) {
-  console.error(pkg.version);
+  console.error(pkg.version); // eslint-disable-line no-console
   process.exit(1);
 }
 
@@ -32,22 +31,22 @@ lookup(args, function(err, res) {
     if (args.json) {
       err = JSON.stringify(err, null, 2);
     }
-    console.error(chalk.red(figures.cross), err.message);
-    console.error(chalk.gray(err.stack));
+    console.error(chalk.red(figures.cross), err.message); // eslint-disable-line no-console
+    console.error(chalk.gray(err.stack)); // eslint-disable-line no-console
     process.exit(1);
     return;
   }
   if (args.json) {
-    console.log(JSON.stringify(res, null, 2));
+    console.log(JSON.stringify(res, null, 2)); // eslint-disable-line no-console
   } else {
     if (res.length === 0) {
-      console.log('☹ No MongoDB instances running');
+      console.log('☹ No MongoDB instances running'); // eslint-disable-line no-console
       return;
     }
-    console.log(chalk.green(figures.tick),
+    console.log(chalk.green(figures.tick), // eslint-disable-line no-console
       ' Yep!', res.length, 'MongoDB instance(s) running:');
     res.map(function(d, i) {
-      console.log('  ', i + 1 + '.',
+      console.log('  ', i + 1 + '.',  // eslint-disable-line no-console
         'port',
         chalk.bold(d.port),
         'with pid',
